d78f8cf8a1003770c1327a5f1375adab55302e7a
golu7679 <golurajak811@gmail.com>
2022-09-22 00:05:32 +0530

    project setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tinku-wallet',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
