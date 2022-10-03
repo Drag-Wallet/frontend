b22ad7c52360aa44fd690c25d8f57ff59bfa5be5
Abhijit Dey <abhijit@inocentum.in>
2022-10-03 23:27:22 +0530

    Update Log 03-10-2022: 	Update: Login page design completed. 	New: Profile update page added and design completed.

---
+++ src/app/auth/profile-edit/profile-edit.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-profile-edit',
+  templateUrl: './profile-edit.page.html',
+  styleUrls: ['./profile-edit.page.scss'],
+})
+export class ProfileEditPage implements OnInit {
+
+  constructor(private navCtrl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  goBack() {
+    this.navCtrl.back();
+  }
+
+}
